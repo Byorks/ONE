1e2447f2f5d7a1367128a8c06448595c78e5abe1
Byorks <vanessabyorksp@gmail.com>
2025-08-02 16:40:35 -0300

    :sparkles: feat: desafios pt1

---
+++ LogicaProgramacaoJavascript/logica-js-projeto_inicial/app-new.js
@@ -0,0 +1,14 @@
+alert('Boas vindas ao jogo do número secreto');
+
+// Criando variável
+let numeroSecreto = 29;
+
+let chute = prompt('Escolha um número entre 1 e 30');
+
+// Importante simular erros para verificar possíveis tentativas do usuário
+
+// Condicional If(Se)
+
+if (numeroSecreto == chute ) {
+
+}
---
+++ LogicaProgramacaoJavascript/logica-js-projeto_inicial/desafios-js/01.js
@@ -0,0 +1,33 @@
+// 1 
+alert("Boas vindas ao nosso site!");
+
+// 2
+let nome =  "Lua";
+
+// 3
+let idade = 24;
+
+// 4
+let numeroDeVendas = 50;
+
+// 5
+let saldoDisponivel = 1000;
+
+// 6 
+alert("Erro! Preencha todos os campos");
+
+// 7
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro);
+
+// 8
+let nomeUsuario = prompt("Digite o seu nome");
+
+// 9
+
+let idadeUsuario = prompt("Digite a sua idade");
+
+//10
+if (idadeUsuario >= 18 ) {
+    alert("Pode tirar a habilitação!");
+}
