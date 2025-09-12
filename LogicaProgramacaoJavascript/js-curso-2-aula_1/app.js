0549233be84578faa15ca02479871b5fb1107d8e
Byorks <vanessabyorksp@gmail.com>
2025-09-11 23:43:28 -0300

    feat: jogo adivinha parte 2

--- LogicaProgramacaoJavascript/js-curso-2-aula_1/app.js
+++ LogicaProgramacaoJavascript/js-curso-2-aula_1/app.js
@@ -3,20 +3,52 @@
 
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-let numeroSecreto;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
-  let campo = document.querySelector(tag)
+  let campo = document.querySelector(tag);
   campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function limparCampo() {
+  chute = document.querySelector('input');
+  chute.value = ''
+}
+
+function reiniciarJogo() {
+  numeroSecreto = gerarNumeroAleatorio();
+  limparCampo();
+  tentativas = 1;
+  exibirMensagemInicial();
+}
+
+function exibirMensagemInicial () {
+  exibirTextoNaTela('h1', 'Jogo do número secreto');
+  exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+  document.getElementById('reiniciar').setAttribute('disabled', true)
+}
 
 function verificarChute() {
-  console.log('O botão foi clicado!');
+  let chute = document.querySelector('input').value;
+  console.log(chute == numeroSecreto);
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela('h1', 'Acertou!');
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+    exibirTextoNaTela('p',  mensagemTentativas);
+    document.getElementById('reiniciar').removeAttribute('disabled');
+  } else { 
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela('p', 'Número secreto é menor');
+    } else {
+      exibirTextoNaTela('p', 'Número secreto é maior');
+    }
+    tentativas++;
+    limparCampo()
+  }
 }
 
 function gerarNumeroAleatorio() {
-  Math.random() * 10 + 1
+  return parseInt(Math.random() * 10 + 1);
 }
